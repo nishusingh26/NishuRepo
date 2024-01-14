671aeaa35c2ead9dc5a27e41bf6d0529a49a08e8
Nishu Singh <79636190+nishusingh26@users.noreply.github.com>
2024-01-14 13:11:56 +0530

    Update sayHello.groovy

--- vars/sayHello.groovy
+++ vars/sayHello.groovy
@@ -1,5 +1,9 @@
 #!/usr/bin/env groovy
 
-def call(String name = 'human') {
+def call(String name = 'Nishu') {
+  echo "Hello, ${name}."
+}
+
+def calling(String name = 'World') {
   echo "Hello, ${name}."
 }
