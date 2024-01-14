059b3161a56b69cdd7214545964e5ae84cae3355
Nishu Singh <79636190+nishusingh26@users.noreply.github.com>
2024-01-14 15:58:21 +0530

    Update GitLeaks.groovy

--- vars/GitLeaks.groovy
+++ vars/GitLeaks.groovy
@@ -1,1 +1,10 @@
 
+stage('Run gitleaks') {
+            steps {
+                script {
+                    // Use 'sh' for Unix-based agents or 'bat' for Windows agents
+                    def command = isUnix() ? 'gitleaks detect -v' : 'gitleaks.exe detect -v'
+                    sh command
+                }
+            }
+}
